4ee04f86b17fa26a859415e13c6dcc5455e52fad
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2021-11-13 18:54:11 +0530

    added my database

--- responses.py
+++ responses.py
@@ -1,24 +1,28 @@
 from datetime import datetime
+import json
 
+f = open("database.json", "r")
+s = f.read()
+info = json.loads(s)
 
 def sample_response(input_text):
     user_message = str(input_text).lower()
 
-    if user_message in ("hello", "hi", "sup"):
-        return "Hey, How are you doing?"
+    space_index = user_message.index(' ')
+    username = user_message[0:space_index]
+    password = user_message[space_index + 1:]
+    # print(username)
+    # print(password)
 
-    if user_message in ("who are you?", "who are you"):
-        return "I am divyansh!!"
+    if username in info.keys():
+        if password == info[username]["Password"]:
+            return str(info[username])
+        else:
+            return "Wrong Password"
 
-    if user_message in ("time", "what's the time"):
+    if user_message in ("time"):
         now = datetime.now()
         date_time = now.strftime("%d/%m/%y, %H:%M:%S")
         return str(date_time)
 
-    if user_message in ("who am i?", "hi bot"):
-        return "Hello, kaisi ho mummy?"
-
-    if user_message in "hello, hi, kaise ho?":
-        return "Hello sona!!"
-
-    return "I don't understand what you want to say."
+    return "Invalid Username."
